009ef51b02ff0d439ffcfbed63a22cfe66493ca3
bryce Kratzer <brycekratzer@u.boisestate.edu>
2025-03-29 20:31:25 -0600

    Updating scale

--- static/display.js
+++ static/display.js
@@ -11,7 +11,7 @@
 /**
  * Creating "graph frame" constants for setting up layout
  */
-const zoomSize = 3;
+const zoomSize = 2;
 
 // Margin is our "inner frame"
 const margin = { 
@@ -116,7 +116,7 @@ function createViz(svg, data, config){
         .attr("text-anchor", "middle")
         // Calculations for position of name
         .attr("x", width/2)
-        .attr("y", height + margin.bottom - 20)
+        .attr("y", height + margin.bottom - 5)
         .text("Date");
     
     svg.append("text")
@@ -127,7 +127,7 @@ function createViz(svg, data, config){
         .attr("transform", "rotate(-90)")
 
         // Position of text
-        .attr("y", -margin.left + 25)
+        .attr("y", -margin.left + 10)
         .attr("x", -(height/2))
         .text(config.yAxisLabel);
 
